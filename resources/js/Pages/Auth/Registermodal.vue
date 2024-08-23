67973ffe72d2674cbf57c680c6e68a5b43cae790
markymouseee <sanamamarknoriel9@gmail.com>
2024-08-24 07:40:24 +0800

    Adding registration form

---
+++ resources/js/Pages/Auth/Registermodal.vue
@@ -0,0 +1,27 @@
+<script setup>
+    import { useForm } from '@inertiajs/vue3';
+
+    const form = useForm({
+        fullname: '',
+        usernam: '',
+        email: '',
+        password: '',
+        password_confirm: ''
+    });
+
+    const submit = () => {
+        form.post(route('register'), {
+            onFinish: () => form.reset('password')
+        })
+    };
+</script>
+
+<template>
+    <form >
+        <div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="loginModal" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                
+            </div>
+        </div>
+    </form>
+</template>
